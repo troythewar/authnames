760c172ebae0be0ea7e18ffee14288425bbba57f
troythewar <troythewar24@gmail.com>
2013-11-11 14:06:53 +0530

    Repository names has been fetched

--- fileop.rb
+++ fileop.rb
@@ -1,38 +1,21 @@
 class A
 	def fileope
-	 # begin
-	      #file_path=
 	  		File.readlines('D:\Rails\Access.txt').each do |lines|
           #puts lines
         if (lines.match('#')||lines.match('@'))
-
           else
-            #puts lines
-            names=lines.split(',')
-            puts names
-            #if(lines.match(',')&& lines.match('=')) #Removes the unwanted User permission from HCL_IP_Repos
-              #lines.split('=')
-            #else
-             #  puts lines
-              # lines.split('=')
-            end
-            #puts lines
+            if (lines.match('/'))
+              k=lines.split(':/]')
+              l=k.to_s
+
+              if(l.match(','))
+                puts l
+              end
           end
-        #k=lines.split('=')
-
         end
-
-		#		puts "There is a file with cameralog"
-		
-			#rescue 
-			 # puts "File isn't present"
-			#ensure 
-			 # puts "To open the file"
-			#end
-			
-	end
-
-
+  end
+  end
+end
 foo=A.new()
 foo.fileope
 
