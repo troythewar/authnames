73ad56c8890bcb5b3e97939407d99979703e68e6
troythewar <troythewar24@gmail.com>
2013-11-14 11:34:22 +0530

    Now the new users are in next line. Need to process the data

--- fileop.rb
+++ fileop.rb
@@ -8,8 +8,9 @@ class A
               if lines.match('\[')
                 puts lines
               end
-              g=lines.split('=')[1]
-              puts g
+              g=lines.split('=')[1].to_s
+              l=g.split(',').to_s
+              puts l
               end
           end
         end
