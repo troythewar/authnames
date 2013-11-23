669e1221c8a10b4851e58a2128ecf071baa17301
troythewar <troythewar24@gmail.com>
2013-11-23 12:13:10 +0530

    Need to check

--- fileop.rb
+++ fileop.rb
@@ -1,16 +1,19 @@
 class A
 	def fileope
 	  		File.readlines('D:\Rails\Access.txt').each do |lines|
+         f= File.open('D:\Rails\Move.txt','w')
           if (lines.match('#')||lines.match('@'))
           else
             if (lines.match(':/'))
             else
               if lines.match('\[')
-                puts lines
+               puts lines
               end
               g=lines.split('=')[1].to_s
               l=g.split(',')
-              puts l
+                puts l
+
+
               end
           end
         end
