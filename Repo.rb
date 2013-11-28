0e9c4be56fba5e333855bfe18e40c97ccf2f865d
troythewar <troythewar24@gmail.com>
2013-11-28 16:00:48 +0530

    Major fix on the Repo.rb

--- Repo.rb
+++ Repo.rb
@@ -1,14 +1,17 @@
 class A
 	def fileope
+        repo=File.open('D:\Rails\Username\Repo.txt','w+')
 	  		File.readlines('D:\Rails\Access.txt').each do |lines|
           #puts lines
         if (lines.match('#')||lines.match('@'))
           else
-            if (lines.match('/'))
-              k=lines.split(':/]').to_s
-              if(k.match(','))
-                puts k
-              end
+            if (lines.match(':/]'))
+              #k=lines.split(':/]').to_s
+              #puts k
+              #if(k.match(','))
+                repo.write lines
+                puts lines
+              #end
           end
         end
   end
--- fileop.rb
+++ fileop.rb
@@ -1,20 +1,7 @@
 class A
 	def fileope
-<<<<<<< HEAD
-	  		File.readlines('D:\Rails\Access.txt').each do |lines|
-          #puts lines
-        if (lines.match('#')||lines.match('@'))
-          else
-            if (lines.match('/'))
-              k=lines.split(':/]').to_s
-              if(k.match(','))
-                puts k
-              end
-          end
-        end
-  end
-=======
-        f= File.open('D:\Rails\Move.txt','w') do |f|
+
+        f= File.open('D:\Rails\Move.txt','w+') do |f|
 	  		File.readlines('D:\Rails\Access.txt').each do |lines|
           if (lines.match('#')||lines.match('@'))
             else
@@ -34,14 +21,15 @@ class A
         end
   end
   def join_them
-    p= File.open('D:\Rails\Username\Move.txt','w') do |p|
-    k=File.readlines('D:\Rails\Move.txt','w').each do |read_lines|
-      y=read_lines.split(',')
+    #p= File.open('D:\Rails\Username\Move.txt','w') do |p|
+    File.readlines('D:\Rails\Move.txt').each do |read_lines|
+      #puts read_lines
+      y=read_lines.sub("[groups]","XXXXXXXXXXXX")
       puts y
-      p.write y
-      end
+     # p.write y
+      #end
     end
->>>>>>> username
+
   end
 end
 foo=A.new()
